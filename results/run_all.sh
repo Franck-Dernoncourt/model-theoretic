1deb6ab892384d9b57b9a0b7c0c4f4a06f0586e7
Franck Dernoncourt <franck.dernoncourt@gmail.com>
2016-07-08 18:03:58 -0400

    add QMR

--- results/run_all.sh
+++ results/run_all.sh
@@ -1,7 +1,9 @@
-# Run all experiments
+# Run all experiments on Linux or OS X.
 
 cd ../code
 
+## AD dataset
+# PLSR = Partial Least Squares Regression
 python xp.py -m PLSR -s ad -v ../data/GoogleNews-vectors-negative300-short-onlyspaces.txt > ad_word2vec_plsr.txt & 
 python xp.py -m PLSR -s ad -v ../data/GoogleNews-vectors-negative300-short-retrofit-framenet.txt > ad_word2vec-framenet_plsr.txt & 
 python xp.py -m PLSR -s ad -v ../data/GoogleNews-vectors-negative300-short-retrofit-ppdb.txt > ad_word2vec-ppdb_plsr.txt & 
@@ -9,12 +11,36 @@ python xp.py -m PLSR -s ad -v ../data/GoogleNews-vectors-negative300-short-retro
 python xp.py -m PLSR -s ad -v ../data/GoogleNews-vectors-negative300-short-retrofit-wordnet-synonyms-plus.txt > ad_word2vec-wordnet-plus_plsr.txt & 
 python xp.py -m PLSR -s ad -v random > ad_random_plsr.txt & 
     
+# NN = Nearest Neighbor
 python xp.py -m nn -s ad -v ../data/GoogleNews-vectors-negative300-short-onlyspaces.txt > ad_word2vec_nn.txt & 
 python xp.py -m nn -s ad -v ../data/GoogleNews-vectors-negative300-short-retrofit-framenet.txt > ad_word2vec-framenet_nn.txt & 
 python xp.py -m nn -s ad -v ../data/GoogleNews-vectors-negative300-short-retrofit-ppdb.txt > ad_word2vec-ppdb_nn.txt & 
 python xp.py -m nn -s ad -v ../data/GoogleNews-vectors-negative300-short-retrofit-wordnet-synonyms.txt > ad_word2vec-wordnet_nn.txt & 
 python xp.py -m nn -s ad -v ../data/GoogleNews-vectors-negative300-short-retrofit-wordnet-synonyms-plus.txt > ad_word2vec-wordnet-plus_nn.txt & 
 python xp.py -m nn -s ad -v random > ad_random_nn.txt & 
-    
+
+# mode
 python xp.py -m mode -s ad  > ad_mode.txt & 
 python xp.py -m true-mode -s ad > ad_true-mode.txt & 
+
+
+# QMR dataset
+# PLSR = Partial Least Squares Regression
+python xp.py -m PLSR -s qmr -v ../data/GoogleNews-vectors-negative300-short-onlyspaces.txt > ad_word2vec_plsr.txt & 
+python xp.py -m PLSR -s qmr -v ../data/GoogleNews-vectors-negative300-short-retrofit-framenet.txt > ad_word2vec-framenet_plsr.txt & 
+python xp.py -m PLSR -s qmr -v ../data/GoogleNews-vectors-negative300-short-retrofit-ppdb.txt > ad_word2vec-ppdb_plsr.txt & 
+python xp.py -m PLSR -s qmr -v ../data/GoogleNews-vectors-negative300-short-retrofit-wordnet-synonyms.txt > ad_word2vec-wordnet_plsr.txt & 
+python xp.py -m PLSR -s qmr -v ../data/GoogleNews-vectors-negative300-short-retrofit-wordnet-synonyms-plus.txt > ad_word2vec-wordnet-plus_plsr.txt & 
+python xp.py -m PLSR -s qmr -v random > ad_random_plsr.txt & 
+
+# NN = Nearest Neighbor
+python xp.py -m nn -s qmr -v ../data/GoogleNews-vectors-negative300-short-onlyspaces.txt > ad_word2vec_nn.txt & 
+python xp.py -m nn -s qmr -v ../data/GoogleNews-vectors-negative300-short-retrofit-framenet.txt > ad_word2vec-framenet_nn.txt & 
+python xp.py -m nn -s qmr -v ../data/GoogleNews-vectors-negative300-short-retrofit-ppdb.txt > ad_word2vec-ppdb_nn.txt & 
+python xp.py -m nn -s qmr -v ../data/GoogleNews-vectors-negative300-short-retrofit-wordnet-synonyms.txt > ad_word2vec-wordnet_nn.txt & 
+python xp.py -m nn -s qmr -v ../data/GoogleNews-vectors-negative300-short-retrofit-wordnet-synonyms-plus.txt > ad_word2vec-wordnet-plus_nn.txt & 
+python xp.py -m nn -s qmr -v random > ad_random_nn.txt & 
+
+# mode
+python xp.py -m mode -s qmr  > ad_mode.txt & 
+python xp.py -m true-mode -s qmr > ad_true-mode.txt & 
